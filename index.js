24488f94d2c88348cd6905648f22bdb5e8e1f79d
Sam Verschueren <sam.verschueren@gmail.com>
2016-03-30 09:07:36 +0200

    add trailing slash to scoped packages

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict';
 module.exports = function (scope) {
 	var rc = require('rc')('npm', {registry: 'https://registry.npmjs.org/'});
-	return rc[scope + ':registry'] || (rc.registry.slice(-1) !== '/' ? rc.registry + '/' : rc.registry);
+	return appendTrailingSlash(rc[scope + ':registry'] || rc.registry);
 };
+
+function appendTrailingSlash(str) {
+	return str.slice(-1) === '/' ? str : str + '/';
+}
