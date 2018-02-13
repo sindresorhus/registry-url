f1d7902b48a294b1f1e4abff5b5eb8373d5a47f7
Jason Kohles <email@jasonkohles.com>
2018-02-13 18:36:21 -0500

    Detect npm_config_registry from the environment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = scope => {
 	const rc = require('rc')('npm', {registry: 'https://registry.npmjs.org/'});
-	const url = rc[scope + ':registry'] || rc.registry;
+	const url = rc[scope + ':registry'] || rc.config_registry || rc.registry;
 	return url.slice(-1) === '/' ? url : url + '/';
 };
