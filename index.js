c322d7a37a31945a90394de6220fcec92a6fb9c3
zanminkian <hellozmj@qq.com>
2025-04-02 09:22:04 +0000

    Remove `rc`

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-import rc from 'rc';
+import {readFileSync} from 'node:fs';
+import {findUpSync} from 'find-up-simple';
+import {parse} from 'ini';
 
 export default function registryUrl(scope) {
-	const result = rc('npm', {registry: 'https://registry.npmjs.org/'});
-	const url = result[`${scope}:registry`] || result.config_registry || result.registry;
-	return url.slice(-1) === '/' ? url : `${url}/`;
+	const defaultRegistry = 'https://registry.npmjs.org/';
+	const npmRcPath = findUpSync('.npmrc');
+	if (!npmRcPath) {
+		return process.env.npm_config_registry || defaultRegistry;
+	}
+
+	const content = readFileSync(npmRcPath, 'utf8');
+	const result = parse(content);
+	const url = result[`${scope}:registry`] || process.env.npm_config_registry || result.registry || defaultRegistry;
+	return url.endsWith('/') ? url : `${url}/`;
 }
