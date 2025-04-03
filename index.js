58a9dc7d1e39d6c83b009f51115f96bd2c5d09a7
zanminkian <hellozmj@qq.com>
2025-04-03 15:37:03 +0800

    Remove `rc` dependency and target Node.js 18 (#22)

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-import rc from 'rc';
+import {readFileSync} from 'node:fs';
+import process from 'node:process';
+import {findUpSync} from 'find-up-simple';
+import {parse} from 'ini';
+
+const normalize = url => url.endsWith('/') ? url : `${url}/`;
 
 export default function registryUrl(scope) {
-	const result = rc('npm', {registry: 'https://registry.npmjs.org/'});
-	const url = result[`${scope}:registry`] || result.config_registry || result.registry;
-	return url.slice(-1) === '/' ? url : `${url}/`;
+	const defaultRegistry = 'https://registry.npmjs.org/';
+	const npmRcPath = findUpSync('.npmrc');
+	if (!npmRcPath) {
+		return normalize(process.env.npm_config_registry || defaultRegistry);
+	}
+
+	const content = readFileSync(npmRcPath, 'utf8');
+	const result = parse(content);
+	return normalize(result[`${scope}:registry`] || process.env.npm_config_registry || result.registry || defaultRegistry);
 }
